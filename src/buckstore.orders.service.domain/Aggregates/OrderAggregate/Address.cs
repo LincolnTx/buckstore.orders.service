3da3f8235e960e1aa210c74cc16ce902a6060851
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-03-15 10:24:30 -0300

    feat: start order aggregate

---
+++ src/buckstore.orders.service.domain/Aggregates/OrderAggregate/Address.cs
@@ -0,0 +1,22 @@
+﻿using buckstore.orders.service.domain.SeedWork;
+
+namespace buckstore.orders.service.domain.Aggregates.OrderAggregate
+{
+    public class Address : ValueObject
+    {
+        public string Street { get; private set; }
+        public string ZipCode { get; private set; }
+        public string District { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+
+        public Address(string street, string zipCode, string district, string city, string state)
+        {
+            Street = street;
+            ZipCode = zipCode;
+            District = district;
+            City = city;
+            State = state;
+        }
+    }
+}
---
+++ src/buckstore.orders.service.domain/Aggregates/OrderAggregate/IOrderRepository.cs
@@ -0,0 +1,9 @@
+﻿using buckstore.orders.service.domain.SeedWork;
+
+namespace buckstore.orders.service.domain.Aggregates.OrderAggregate
+{
+    public interface IDeliveryRepository : IRepository<Delivery>
+    {
+        
+    }
+}
