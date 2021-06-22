ea14616364a1e4b8075fe62d38bc6f348911d91d
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-06-22 11:14:16 -0300

    feat: add envs for kafka and create integration events for consumers

---
+++ src/buckstore.orders.service.application/IntegrationEvents/ProductDeletedIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace buckstore.orders.service.application.IntegrationEvents
+{
+    public class ProductDeletedIntegrationEvent : IntegrationEvent
+    {
+        public Guid Id { get; set; }
+
+        public ProductDeletedIntegrationEvent(Guid id) : base(DateTime.Now)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ src/buckstore.orders.service.infrastructure.environment/Configuration/KafkaConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace buckstore.orders.service.infrastructure.environment.Configurations
+{
+    public class KafkaConfiguration
+    {
+        public string ConnectionString { get;set; }
+        public string Group { get;set; }
+        public string ManagerToOrdersCreate { get;set; }
+        public string ManagerToOrdersUpdate { get;set; }
+        public string ManagerToOrdersDelete { get;set; }
+        public string OrdersToManager { get;set; }
+        public string OrdersToProducts { get;set; }
+    }
+}
