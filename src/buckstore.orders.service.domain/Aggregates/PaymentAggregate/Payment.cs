682b6f73757c6254a3bb692ec35f0909283a1967
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-06-22 10:33:07 -0300

    feat: preparing payment aggregate

---
+++ src/buckstore.orders.service.domain/Aggregates/PaymentAggregate/Payment.cs
@@ -0,0 +1,23 @@
+﻿namespace buckstore.orders.service.domain.Aggregates.PaymentAggregate
+{
+    public class Payment
+    {
+        private string _creditCard;
+        public string CreditCardEnd => HandleCreditCardEnd();
+        private int _paymentStatusId;
+        public PaymentStatus PaymentStatus { get; }
+
+        public Payment(string creditCard)
+        {
+            _creditCard = creditCard;
+            _paymentStatusId = PaymentStatus.Pending.Id;
+            PaymentStatus = PaymentStatus.Pending;
+        }
+
+        private string HandleCreditCardEnd()
+        {
+            return string.IsNullOrEmpty(_creditCard) ? string.Empty 
+                : _creditCard.Substring(_creditCard.Length - 4);
+        }
+    }
+}
