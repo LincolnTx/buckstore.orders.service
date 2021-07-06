353f9f5cb8e3db7295aa40d23d78b51baedae7ab
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-07-06 14:56:32 -0300

    feat: start payment api integration
    Create a proxy fot the payment api
    Create a DTO

---
+++ src/buckstore.orders.service.application/DTOs/CreditCarPaymentDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace buckstore.orders.service.application.DTOs
+{
+    public class CreditCarPaymentDto
+    {
+        public string CardNumber { get; set; }
+        public DateTime CardExpiration { get; set; }
+        public string CardSecurityNumber { get; set; }
+        public string CardHolderName { get; set; }
+        public decimal OrderAmount { get; set; }
+        
+    }
+}
