3b35e27112646783be0f3cb3f42960c2010e43a3
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-07-18 14:10:37 -0300

    feat: start list all orders with pagination

--- src/buckstore.orders.service.application/DTOs/GetOrdersResponseDto.cs
+++ src/buckstore.orders.service.application/DTOs/GetOrdersResponseDto.cs
@@ -6,6 +6,11 @@ namespace buckstore.orders.service.application.DTOs
     public class GetOrdersResponseDto
     {
         public IEnumerable<OrderResponseDto> Orders { get; set; }
+
+        public GetOrdersResponseDto(IEnumerable<OrderResponseDto> orders)
+        {
+            Orders = orders;
+        }
     }
 
     public class OrderResponseDto
---
+++ src/buckstore.orders.service.application/Queries/ListOrdersQuery.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using MediatR;
+using buckstore.orders.service.application.DTOs;
+
+namespace buckstore.orders.service.application.Queries
+{
+    public class ListOrdersQuery : IRequest<GetOrdersResponseDto>
+    {
+        private const int PageSize = 10;
+        public Guid BuyerId { get; set; }
+        public List<string> StatusFilter { get; set; }
+        public int Page { get; set; }
+
+        public ListOrdersQuery(Guid buyerId, List<string> statusFilter, int page)
+        {
+            BuyerId = buyerId;
+            StatusFilter = ValidateArrays(statusFilter);
+            Page = page > 0 ? (page - 1 ) * PageSize : 0;
+        }
+        
+        List<string> ValidateArrays(List<string> values)
+        {
+            if (values.Any() && values[0].Contains(','))
+            {
+                return values[0].Split(',').ToList();
+            }
+
+            return values;
+        }
+    }
+}
