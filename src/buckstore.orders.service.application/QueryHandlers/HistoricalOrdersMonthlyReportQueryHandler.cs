c798d70fda00a14dc9f17de8440f5a9101c5dd1a
Lincoln Teixeira <lincolnsf98@gmail.com>
2021-11-18 10:34:43 -0300

    refactor: little changes to make code cleaner

--- src/buckstore.orders.service.application/QueryHandlers/HistoricalOrdersMonthlyReportQueryHandler.cs
+++ src/buckstore.orders.service.application/QueryHandlers/HistoricalOrdersMonthlyReportQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using MediatR;
 using System.Threading;
 using System.Threading.Tasks;
@@ -11,18 +12,12 @@ namespace buckstore.orders.service.application.QueryHandlers
 {
     public class HistoricalOrdersMonthlyReportQueryHandler : QueryHandler, IRequestHandler<HistoricalOrdersMonthlyReportQuery, HistoricalMonthlyReportDto>
     {
-        public async Task<HistoricalMonthlyReportDto
-        > Handle(HistoricalOrdersMonthlyReportQuery request, CancellationToken cancellationToken)
+        public async Task<HistoricalMonthlyReportDto> Handle(HistoricalOrdersMonthlyReportQuery request, CancellationToken cancellationToken)
         {
             using var dbConnection = DbConnection;
 
-            const string sqlCommand = "SELECT to_char(date_trunc('month', o.\"OrderDate\"), 'YYYY') AS Year, " +
-                                      "to_char(date_trunc('month', o.\"OrderDate\"), 'Mon') AS Month, " +
-                                      "to_char(date_trunc('month', o.\"OrderDate\"), 'MM') AS MonthNumber, " +
-                                      "sum(o.value) AS MonthlySum FROM \"order\" o " +
-                                      "WHERE o.\"OrderStatusId\" = @statusId " +
-                                      "GROUP BY date_trunc('month', o.\"OrderDate\") " +
-                                      "ORDER BY date_trunc('month', o.\"OrderDate\")";
+            var sqlCommand = QueryBuilder(request.StatusIdFilter);
+           
             try
             {
                 var data = await dbConnection.QueryAsync<HistoricalOrderMonthlyViewModel>(sqlCommand, new
@@ -37,7 +32,26 @@ namespace buckstore.orders.service.application.QueryHandlers
                 Console.WriteLine(e);
                 throw;
             }
-                
+        }
+
+        private string QueryBuilder(int statusFilter)
+        {
+            var query = new StringBuilder();
+
+            query.Append("SELECT to_char(date_trunc('month', o.\"OrderDate\"), 'YYYY') AS Year, ");
+            query.Append("to_char(date_trunc('month', o.\"OrderDate\"), 'Mon') AS Month, ");
+            query.Append("to_char(date_trunc('month', o.\"OrderDate\"), 'MM') AS MonthNumber, ");
+            query.Append("sum(o.value) AS MonthlySum FROM \"order\" o ");
+
+            if (statusFilter != 0)
+            {
+                query.Append("WHERE o.\"OrderStatusId\" = @statusId ");
+            }
+
+            query.Append("GROUP BY date_trunc('month', o.\"OrderDate\") ");
+            query.Append("ORDER BY date_trunc('month', o.\"OrderDate\")");
+
+            return query.ToString();
         }
     }
 }
